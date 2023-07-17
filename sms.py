fce4a01f4038dcd0673978b7924f9d7094c6fb34
Your Name <you@example.com>
2023-07-17 03:43:52 +0200

    first commit

---
+++ sms.py
@@ -0,0 +1,31 @@
+import requests
+
+from termcolor import colored
+from pyfiglet import figlet_format
+
+text = figlet_format("Send SMS", font="big")
+print(colored(text, "red"))
+print("Created by Your Name")
+
+number = int(input(colored("[+] Número: ", "green")))
+message = input(colored("[+] Mensaje: ", "green"))
+
+resp = requests.post('https://textbelt.com/text', {
+
+  'phone': number,
+  'message': message,
+  'key': 'textbelt',
+
+})
+
+response = resp.json()
+
+print ("")
+
+if response["success"] == "true":
+   print (colored("El mensaje se ha enviado con éxito", "green"))
+else:
+   print (colored("No se pudo enviar el mensaje por la siguiente razón:", "red"))
+   print ("")
+   print (colored(response["error"], "red"))
+
